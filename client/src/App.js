6fe4ea4ef0b9e4bca00047e94911aec154a15a10
Kyle Van Bergen <kyleavb@gmail.com>
2019-11-21 13:07:39 -0800

    update folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Home from './majors/Home'
+import Feed from './majors/Feed'
+import EventList from './majors/EventList'
+import SignUp from './majors/SignUp'
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -13,21 +17,15 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/signup' component={SignUp} />
+            <Route exact path='/:guid' component={Feed} />
+            <Route exact path='/:guid/events' component={EventList} />
+          </Switch>
+        </Router>
       </div>
     );
   }
