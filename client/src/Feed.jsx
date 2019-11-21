ed9f257658dcb555d0fbbaef5daacf96855debec
sross9845 <sean.ross.985@gmail.com>
2019-11-21 12:51:50 -0800

    first commit

---
+++ client/src/Feed.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Feed extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <p> This is the Feed Route </p>
+         );
+    }
+}
+
+export default Feed;
