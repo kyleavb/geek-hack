485edca3a4eed8c1715abc3ac56a13eaff25faae
Kyle Van Bergen <kyleavb@gmail.com>
2019-11-21 11:44:33 -0800

    initial commit

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const app = require('express')();
+const express = require('express');
+const path = require( 'path' );
+const config = require('./config');
+const routes = require('./routes');
+
+// Serve the static files from the React app
+// app.use(express.static(path.resolve(__dirname, '/data')));
+// app.use(express.static(path.join( __dirname, '../', 'client', 'build')));
+
+app.get('/data', ( req, res ) => {
+    console.log('client request');
+    res.sendStatus(200);
+});
+
+
+app.all( '/*', routes );
+// common 404
+app.use( (req, res) => res.status( 404 ).send('Shit Broke, Not Found') );
+
+const listen = ( log ) => {
+    return app.listen( config.PORT, () => {
+        console.log( log );
+    });
+  }
+
+module.exports.init = () => {
+    return listen( `Server listening on port ${ config.PORT }.` );
+};
